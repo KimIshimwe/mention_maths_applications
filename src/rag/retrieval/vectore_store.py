ced06b99ecf44417db7efc8d3878f6b91a64b683
KimIshimwe <kim.ishimwe@univ-tlse3.fr>
2026-09-25 14:41:50 +0200

    base de données vectorielle

---
+++ src/rag/retrieval/vectore_store.py
@@ -0,0 +1,16 @@
+from langchain_qdrant import Qdrant
+from qdrant_client import QdrantClient
+from src.core.config import settings
+from src.rag.ingestion.indexers import get_hf_embeddings
+
+def get_base_retriever(collection_name: str= "formation_docs"):
+    client = QdrantClient(url=settings.qdrant_url)
+    qdrant = Qdrant(
+        client = client,
+        collection_name=collection_name,
+        embeddings = get_hf_embeddings()
+        
+    )
+    return qdrant.as_retriever(search_kwargs={"k":20})
+
+    
