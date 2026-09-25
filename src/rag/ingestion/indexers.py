70164cdbfe4b015b8f46d2f404f55092ce335a85
KimIshimwe <kim.ishimwe@univ-tlse3.fr>
2026-09-25 14:24:44 +0200

    test code + ajout api llama_parse

--- src/rag/ingestion/indexers.py
+++ src/rag/ingestion/indexers.py
@@ -1,1 +1,19 @@
 from langchain_qdrant import Qdrant
+from langchain_huggingface import HuggingFaceEmbeddings
+from src.core.config import settings
+
+def get_hf_embeddings():
+     return HuggingFaceEmbeddings(
+         model = "BAAI/bge-m3",
+         huggingfacehub_api_token = settings.huggingface_api_key
+     )
+     
+def index_chunks(chunks, collection_name: str = "formations_docs"):
+    
+    Qdrant.from_documents(
+        chunks,
+        get_hf_embeddings(),
+        url = settings.qdrant_url,
+        collection_name = collection_name
+    
+    )
