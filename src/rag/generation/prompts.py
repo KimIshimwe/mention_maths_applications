939d39d1b5fe138a5284de3fcde0d45ec5a8b227
KimIshimwe <kim.ishimwe@univ-tlse3.fr>
2026-09-25 15:05:46 +0200

    Partie génération

---
+++ src/rag/generation/prompts.py
@@ -0,0 +1,14 @@
+from langchain_core.prompts import PromptTemplate
+
+def get_formation_prompt():
+    template = """Vous agissez en tant qu'expert en orientation pour les étudiants de la mention Mathématiques et applications de l'Université de Toulouse.
+    Répondez à la question en vous basant de manière stricte sur les brochures des différentes parcours présentes dans cette mention
+    Si la réponse ne figure pas dans le contexte, indiquez-le clairement. N'utilisez pas d'informations provenant d'autres sources.
+    Contexte:
+    {context}
+    
+    Question utilisateur: {question}
+    
+    Réponse:"""
+    
+    return PromptTemplate.from_template(template)
